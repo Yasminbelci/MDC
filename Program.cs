4cd6129bf2817d1b4dcbaad5de018529e1ea6655
yasmin <yasmin.belci13@gmail.com>
2022-05-27 19:40:41 -0300

    yasmin belci

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿double a, b, dividendo, divisor, resto;
+
+Console.Clear();
+
+Console.WriteLine("--- Máximo divisor comum ---\n");
+
+Console.Write("Digite o primeiro número (a): ");
+a = dividendo = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o segundo número (b): ");
+b = divisor = Convert.ToDouble(Console.ReadLine());
+
+do
+{
+    resto = dividendo % divisor;
+    dividendo = divisor;
+    divisor = resto;
+} 
+while (resto != 0);
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"\nMDC({a}, {b}) = {dividendo}");
+Console.ResetColor();
+
+Console.Write("\nPressione uma tecla para finalizar... ");
+Console.ReadKey(); 
+Console.Clear();
